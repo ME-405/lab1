d1ff092a8d0211545e80394fcfd42198644293e9
Jacob <jacob.bograd@gmail.com>
2022-01-23 13:34:16 -0800

    Updated Documentation

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-###### This documentation is for Lab #1 
+##Cal Poly ME-405
+#Lab 01
+Jacob Bograd, Nick De Simone, Horacio Albarran
+
+This lab is a basic Motor and Encoder class written in MicroPython
+
+Doxygen Documentation
+https://me-405.github.io/lab1/
--- docs/search/all_4.js
+++ docs/search/all_4.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['main_5',['main',['../namespacemain.html',1,'']]],
-  ['motor_6',['Motor',['../namespace_motor.html',1,'']]],
-  ['motordriver_7',['MotorDriver',['../class_motor_1_1_motor_driver.html',1,'Motor']]]
+  ['main_0',['main',['../namespacemain.html',1,'']]],
+  ['motor_1',['Motor',['../namespace_motor.html',1,'']]],
+  ['motordriver_2',['MotorDriver',['../class_motor_1_1_motor_driver.html',1,'Motor']]]
 ];
